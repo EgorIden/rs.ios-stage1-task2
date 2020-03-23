6d0a17bf021b75210590bfed4a25ac4b7d123895
Berty <hatebreed1991@mail.ru>
2020-03-23 23:56:07 +0700

    feat: add solution for 3 task

--- RSSchool_T2/RSSchool_T2/Exercises/3/AbbreviationMaker.m
+++ RSSchool_T2/RSSchool_T2/Exercises/3/AbbreviationMaker.m
@@ -3,6 +3,22 @@
 @implementation AbbreviationMaker
 // Complete the abbreviation function below.
 - (NSString *) abbreviationFromA:(NSString *)a toB:(NSString *)b {
-    return @"Test";
+    
+    NSString *upperA = [a uppercaseString];
+
+    NSString *result = @"";
+    
+    for (int i=0; i<upperA.length; i++) {
+        char subsA = [upperA characterAtIndex:i];
+        for (int q=0; q<b.length; q++) {
+            char subsB = [b characterAtIndex:q];
+            if (subsB==subsA) {
+                NSString *addChar = [NSString stringWithFormat:@"%c", subsA];
+                result = [result stringByAppendingString:(addChar)];
+            }
+        }
+    }
+    
+    return [result isEqualToString:b] ? @"YES" : @"NO";
 }
 @end
